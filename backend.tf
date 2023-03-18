7f9be2bdca687db8d419e8cd300c41163edd5f2c
kaysplit07 <62314720+kaysplit07@users.noreply.github.com>
2023-03-18 09:33:23 -0500

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket = "my-aws-tf-state-bucket"
+    bucket = "mytestapibucket"
     key = "main"
     region = "us-east-1"
-    dynamodb_table = "my-dynamo-db-table"
+    dynamodb_table = "Teraform-lock"
   }
 }
