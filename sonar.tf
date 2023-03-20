2ccb063942838c077a2d08cd7055c85f64b84ae9
Philip Anyaehie <123973997+philawzz@users.noreply.github.com>
2023-03-20 20:10:17 +0000

    Update sonar.tf

--- sonar.tf
+++ sonar.tf
@@ -1,17 +1,15 @@
+
 resource "aws_instance" "mySonarInstance" {
-      ami           = "ami-0557a15b87f6559cf"
+      ami           = "ami-0aaa5410833273cfe"
+      subnet_id         = aws_subnet.main-subnet.id
       key_name = var.key_name
       instance_type = "t2.micro"
-      #vpc_id = data.aws_vpc.selected.id
+      
       vpc_security_group_ids = [aws_security_group.sonar-sg-2022.id]
       tags= {
         Name = "sonar_instance"
       }
     }
-
-# data "aws_vpc" "selected" {
-#   id = "vpc-043007f00a536bcbf"
-# }	
 
  resource "aws_security_group" "sonar-sg-2022" {
       name        = "security_sonar_group_2022"
@@ -45,11 +43,4 @@ resource "aws_instance" "mySonarInstance" {
       }
     }
 
-# Create Elastic IP address for Sonar instance
-resource "aws_eip" "mySonarInstance" {
-  vpc      = true
-  instance = aws_instance.mySonarInstance.id
-tags= {
-    Name = "sonar_elastic_ip"
-  }
-}
+
