9dbf6bdbabc966c933208dc1517ef3d53333540a
Philip Anyaehie <123973997+philawzz@users.noreply.github.com>
2023-03-20 21:05:16 +0000

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -5,7 +5,7 @@ variable "aws_region" {
 
 variable "key_name" { 
     description = " SSH keys to connect to ec2 instance" 
-    default     =  "London02.pem" 
+    default     =  "London02" 
 }
 
 variable "instance_type" { 
